338cd2d0d3d9c9a9d7aedcd729fd011b76c10fbc
ronsun <ron.sun@hotmail.com.tw>
2021-12-12 01:31:13 +0800

    First Version

---
+++ MoreNet.Cryptography/Extensions/HashAlgorithmExtensions.cs
@@ -0,0 +1,46 @@
+﻿using System.Text;
+
+namespace System.Security.Cryptography
+{
+    /// <summary>
+    /// Extension methods for HashAlgorithm.
+    /// </summary>
+    public static class HashAlgorithmExtensions
+    {
+        /// <summary>
+        /// CompteHash and convert result to hex string without charactor '-'.
+        /// </summary>
+        /// <param name="hash">The instance of HashAlgorithm. </param>
+        /// <param name="plaintext">Plaintext, usd UTF-8 as default encoding. </param>
+        /// <returns>Hashed string.</returns>
+        public static string ComputeHashToHex(this HashAlgorithm hash, string plaintext)
+        {
+            return ComputeHashToHex(hash, plaintext, Encoding.UTF8);
+        }
+
+        /// <summary>
+        /// CompteHash and convert result to hex string without charactor '-'.
+        /// </summary>
+        /// <param name="hash">The instance of HashAlgorithm. </param>
+        /// <param name="plaintext">Plaintext. </param>
+        /// <param name="plaintextEncoding">Encoding of plaintext.</param>
+        /// <returns>Hashed string.</returns>
+        public static string ComputeHashToHex(this HashAlgorithm hash, string plaintext, Encoding plaintextEncoding)
+        {
+            return ComputeHashToHex(hash, plaintextEncoding.GetBytes(plaintext));
+        }
+
+        /// <summary>
+        /// CompteHash and convert result to hex string without charactor '-'.
+        /// </summary>
+        /// <param name="hash">The instance of HashAlgorithm. </param>
+        /// <param name="plaintextBytes">Plaintext in byte array. </param>
+        /// <returns>Hashed string.</returns>
+        public static string ComputeHashToHex(this HashAlgorithm hash, byte[] plaintextBytes)
+        {
+            byte[] ciphertextBytes = hash.ComputeHash(plaintextBytes);
+
+            return BitConverter.ToString(ciphertextBytes).Replace("-", string.Empty);
+        }
+    }
+}
---
+++ MoreNet.Cryptography/Extensions/SymmetricAlgorithmExtensions.cs
@@ -0,0 +1,118 @@
+﻿using System.IO;
+using System.Text;
+
+namespace System.Security.Cryptography
+{
+    /// <summary>
+    /// Extension methods for <see cref="SymmetricAlgorithm"/>.
+    /// </summary>
+    public static class SymmetricAlgorithmExtensions
+    {
+        /// <summary>
+        /// Encrypt to base64 string.
+        /// </summary>
+        /// <param name="symmetric">The instance of SymmetricAlgorithm. </param>
+        /// <param name="plaintext">The plaintext string by UTF-8 encoding. </param>
+        /// <returns>Ciphertext. </returns>
+        public static string EncryptToBase64(this SymmetricAlgorithm symmetric, string plaintext)
+        {
+            return EncryptToBase64(symmetric, plaintext, Encoding.UTF8);
+        }
+
+        /// <summary>
+        /// Encrypt to base64 string.
+        /// </summary>
+        /// <param name="symmetric">The instance of SymmetricAlgorithm. </param>
+        /// <param name="plaintext">The plaintext string. </param>
+        /// <param name="plaintextEncoding">Encoding of plaintext. </param>
+        /// <returns>Ciphertext. </returns>
+        public static string EncryptToBase64(this SymmetricAlgorithm symmetric, string plaintext, Encoding plaintextEncoding)
+        {
+            return EncryptToBase64(symmetric, plaintextEncoding.GetBytes(plaintext));
+        }
+
+        /// <summary>
+        /// Encrypt to base64 string.
+        /// </summary>
+        /// <param name="symmetric">The instance of SymmetricAlgorithm. </param>
+        /// <param name="plaintextBytes">The plaintext in byte array. </param>
+        /// <returns>Ciphertext. </returns>
+        public static string EncryptToBase64(this SymmetricAlgorithm symmetric, byte[] plaintextBytes)
+        {
+            var ciphertextBytes = Encrypt(symmetric, plaintextBytes);
+            return Convert.ToBase64String(ciphertextBytes);
+        }
+
+        /// <summary>
+        /// Encrypt.
+        /// </summary>
+        /// <param name="symmetric">The instance of SymmetricAlgorithm. </param>
+        /// <param name="plaintextBytes">The plaintext in byte array. </param>
+        /// <returns>Ciphertext bytes. </returns>
+        public static byte[] Encrypt(this SymmetricAlgorithm symmetric, byte[] plaintextBytes)
+        {
+            MemoryStream ms = new MemoryStream();
+            using (CryptoStream cs = new CryptoStream(ms, symmetric.CreateEncryptor(), CryptoStreamMode.Write))
+            {
+                cs.Write(plaintextBytes, 0, plaintextBytes.Length);
+                cs.FlushFinalBlock();
+                return ms.ToArray();
+            }
+        }
+
+        /// <summary>
+        /// Decrypt from base64 string.
+        /// </summary>
+        /// <param name="symmetric">The instance of SymmetricAlgorithm. </param>
+        /// <param name="ciphertext">The ciphertext string. </param>
+        /// <returns>Plaintext. </returns>
+        public static string DecryptFromBase64(this SymmetricAlgorithm symmetric, string ciphertext)
+        {
+            return DecryptFromBase64(symmetric, ciphertext, Encoding.UTF8);
+        }
+
+        /// <summary>
+        /// Decrypt from base64 string.
+        /// </summary>
+        /// <param name="symmetric">The instance of SymmetricAlgorithm. </param>
+        /// <param name="ciphertext">The ciphertext string. </param>
+        /// <param name="plaintextEncoding">Encoding of plaintext. </param>
+        /// <returns>Plaintext. </returns>
+        public static string DecryptFromBase64(this SymmetricAlgorithm symmetric, string ciphertext, Encoding plaintextEncoding)
+        {
+            var ciphertextBytes = Convert.FromBase64String(ciphertext);
+            var plaintextBytes = Decrypt(symmetric, ciphertextBytes);
+            return plaintextEncoding.GetString(plaintextBytes);
+        }
+
+        /// <summary>
+        /// Decrypt.
+        /// </summary>
+        /// <param name="symmetric">The instance of SymmetricAlgorithm. </param>
+        /// <param name="ciphertextBytes">The ciphertext bytes. </param>
+        /// <param name="plaintextEncoding">Encoding of plaintext. </param>
+        /// <returns>Plaintext. </returns>
+        public static string Decrypt(this SymmetricAlgorithm symmetric, byte[] ciphertextBytes, Encoding plaintextEncoding)
+        {
+            var plaintextBytes = Decrypt(symmetric, ciphertextBytes);
+            return plaintextEncoding.GetString(plaintextBytes);
+        }
+
+        /// <summary>
+        /// Decrypt.
+        /// </summary>
+        /// <param name="symmetric">The instance of SymmetricAlgorithm. </param>
+        /// <param name="ciphertextBytes">The ciphertext bytes.</param>
+        /// <returns>Plaintext. </returns>
+        public static byte[] Decrypt(this SymmetricAlgorithm symmetric, byte[] ciphertextBytes)
+        {
+            MemoryStream ms = new MemoryStream();
+            using (CryptoStream cs = new CryptoStream(ms, symmetric.CreateDecryptor(), CryptoStreamMode.Write))
+            {
+                cs.Write(ciphertextBytes, 0, ciphertextBytes.Length);
+                cs.FlushFinalBlock();
+                return ms.ToArray();
+            }
+        }
+    }
+}
